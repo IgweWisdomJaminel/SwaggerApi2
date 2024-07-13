94b38ff7ef911c9450eb2ea34a7b9ee5e9ad5d30
jaminel <jamineltechnology@gmail.com>
2024-07-13 21:16:40 +0100

    new update

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "HngTask3_Update.yaml",
+    url: "IGWEWISDOM20001_1-api-documentation-1.0.0-resolved.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
